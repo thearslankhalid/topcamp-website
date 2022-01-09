aa6ef8c0d8dfda0d541190fb6108c17c8713634f
Arslan Khalid <arslankhalidwar@gmail.com>
2022-01-09 23:20:55 +0500

    New: Added call to action section

---
+++ src/components/common/CtaSection.js
@@ -0,0 +1,19 @@
+import styles from '@/styles/components/common/ctaSection.module.scss'
+
+export default function CTASection() {
+ 
+  return (
+    <section className={styles.ctaSection}>
+      <div className='container'>
+        <div className='row'>
+          <div className={`col ${styles.container}`}>
+            <h1 className={styles.title}>Start your application</h1>
+            <p className={styles.lead}>Become a student at TopCamp. The next application deadline is:</p>
+            <p className={styles.date}>February, 14th 2022</p>
+            <a className={styles.link} href="https://www.google.com">Apply nowa</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.scss'
+import Hero from '@/components/home/Hero'
+import CTASection from '@/components/common/CTAsection'
 
 export default function Home() {
 
-  const offers = [
-    'Industry Experience',
-    'Best practices',
-    'Basics of JavaScript, Git, HTML, and CSS'
-  ]
-
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>TopCamp Website</title>
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.smallCircle}>
-          <Image width={80} height={80} src="/assets/circle-sm.svg" alt="A circle filled with gradient" />
-        </div>
-        <div className={styles.largeCircle}>
-          <Image width={250} height={250} src="/assets/circle-lg.svg" alt="A circle filled with gradient" />
-        </div>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-6'>
-              <header className={styles.header}>
-                <h1 className={styles.title}>
-                  Learn modern
-                  <br />
-                  <span className={styles.gradient}>web development</span>
-                </h1>
-                <p className={styles.lead}>
-                  Get hands on training and learn the basics of the front-end development from the pros.
-                </p>
-                <ul className={styles.offers}>
-                    {
-                      offers.map((offer, index) => (
-                        <li className={styles.offerItem} key={`offer-${index}`}>
-                          <span className="material-icons">
-                            check_circle
-                          </span>
-                          <span>
-                            {offer}
-                          </span>
-                        </li>
-                      ))
-                    }
-                </ul>
-                <form className={styles.subscribeForm}>
-                  <input type="email" required placeholder='Enter your email address' />
-                  <button type='submit'>Enroll Now</button>
-                </form>
-              </header>
-            </div>
-
-            <div className='col-lg-6'>
-              <div className={styles.imageContainer}>
-                <Image width="1000" height="600" src="/assets/coding-header-bg.svg" alt="A developer coding styling div elements" />
-              </div>
-            </div>
-          </div>
-        </div>
+      <main>
+        <Hero />
+        <CTASection />
       </main>
     </div>
   )
