c727a555671cb80459ff65a8ace9362c976022f8
Arslan Khalid <arslankhalidwar@gmail.com>
2022-04-29 01:50:29 +0500

    New: add new landing page sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import TopNav from '@/components/common/TopNav'
 import Hero from '@/components/home/Hero'
 import DiscoverSection from '@/components/home/discoverSection'
+import TimelineSection from '@/components/home/TimelineSection'
 import FaqSection from '@/components/common/FaqSection'
 import CTASection from '@/components/common/CtaSection'
 import Footer from '@/components/common/Footer'
+import InstructorSection from '@/components/home/InstructorSection'
 
 export default function Home() {
 
@@ -18,6 +20,8 @@ export default function Home() {
         <TopNav />
         <Hero />
         <DiscoverSection />
+        <TimelineSection />
+        <InstructorSection />
         <FaqSection />
         <CTASection />
         <Footer />
